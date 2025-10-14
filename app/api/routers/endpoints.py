e33a5ca1bb2e860f0e2ccff038451e218b2355ea
Francesco Grillea <f.grillea@reply.it>
2025-10-14 23:49:04 +0200

    (feat)[srv-configuration-manager]: exception flow

--- app/api/routers/endpoints.py
+++ app/api/routers/endpoints.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, HTTPException
+from rv16_lib.exceptions import RV16Exception
 from rv16_lib.logger import logger
 from starlette import status
 from starlette.responses import JSONResponse
 
 from rv16_lib.configuration_manager.entities import ServiceRegistrationRequest, ServicePairingRequest, ServiceConfigurationRequest
 
+from models.exceptions.exceptions import ConfigurationManagerException
 from service import service as srv
 
 # Create an API Router
@@ -30,17 +32,25 @@ def register_service(request_body: ServiceRegistrationRequest) -> JSONResponse:
         )
 
     provider = srv.get_provider(request_body.provider)
-    response = provider.register_service(service=request_body.service,
-                                         configuration=request_body.configuration)
+    if not provider:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail=f"Provider {request_body.provider} is not available. Choose among {list(srv.providers.keys())}",
+        )
 
-    return JSONResponse(status_code=status.HTTP_200_OK, content=response)
+    try:
+        response = provider.register_service(service=request_body.service,
+                                             configuration=request_body.configuration)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=response)
+    except ConfigurationManagerException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+
 
 
 @router.post("/get-service-configuration")
 def get_service_configuration(request_body: ServiceConfigurationRequest) -> JSONResponse:
 
     if not srv or srv.providers == {}:
-        print(f"Provider {request_body.provider} is not available. No providers registered.")
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             detail="Service is not available",
@@ -48,12 +58,12 @@ def get_service_configuration(request_body: ServiceConfigurationRequest) -> JSON
 
     provider = srv.get_provider(request_body.provider)
     if not provider:
-        print(f"Provider {request_body.provider} is not available. Choose among {list(srv.providers.keys())}")
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             detail=f"Provider {request_body.provider} is not available. Choose among {list(srv.providers.keys())}",
         )
-
-    response = provider.get_service(service=request_body.service)
-    return JSONResponse(status_code=status.HTTP_200_OK, content=response)
-
+    try:
+        response = provider.get_service(service=request_body.service)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=response)
+    except ConfigurationManagerException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
--- app/providers/local.py
+++ app/providers/local.py
@@ -16,23 +16,33 @@ class LocalProvider(Provider):
         self.db_client = db_client
 
     def register_service(self, service: str, configuration: dict):
-        logger.info("Registering service %s with configuration %s", service, configuration)
+        logger.info("Registration started for Service %s with configuration %s", service, configuration)
+        try:
+            payload = RedisElement(
+                key=service,
+                value=json.dumps(configuration)
+            )
+            result = self.db_client.insert_one(payload)
+        except Exception as e:
+            logger.error("Error registering service %s: %s", service, e)
+            raise ConfigurationManagerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                                message=f"Error registering service {service}: {e}")
 
-        payload = RedisElement(
-            key=service,
-            value=json.dumps(configuration)
-        )
-        result = self.db_client.insert_one(payload)
-        logger.info("Service %s registered: %s", service, result)
+        logger.info("Registration completed successfully for Service %s: %s", service, result)
         return result
 
 
     def get_service(self, service: str):
         logger.info(f"Retrieving configuration for service {service}")
-        payload = RedisElement(
-            key=service,
-        )
-        service_params = self.db_client.find(payload)
+        try:
+            payload = RedisElement(
+                key=service,
+            )
+            service_params = self.db_client.find(payload)
+        except Exception as e:
+            logger.error(f"Error retrieving service {service}: {e}")
+            raise ConfigurationManagerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                                message=f"Error retrieving service {service}: {e}")
         if service_params is None:
             raise ConfigurationManagerException(status_code=status.HTTP_404_NOT_FOUND, message=f"Service {service} not found.")
         response = json.loads(service_params)
